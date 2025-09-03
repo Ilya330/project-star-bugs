84ff32e40649a3182b14322a995ce11e9c29eb89
Yevhen <Artuikhov@gmail.com>
2025-09-03 17:16:47 +0300

    fin welcome

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,1 +1,31 @@
-// alert( message: 'hello');
+(() => {
+  const refs = {
+    // Додати атрибут data-modal-open на кнопку відкриття
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    // Додати атрибут data-modal на бекдроп модалки
+    modal: document.querySelector('[data-modal]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.modal.classList.toggle('is-open');
+  }
+
+  const refsmenu = {
+    // Додати атрибут data-modal-open на кнопку відкриття
+    openModalBtn: document.querySelector('[menu-open]'),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeMenuBtn: document.querySelector('[menu-close]'),
+    // Додати атрибут data-modal на бекдроп модалки
+    menu: document.querySelector('[menu]'),
+  };
+  refsmenu.openMenuBtn.addEventListener('click', toggleMenu);
+  refsmenu.closeMenuBtn.addEventListener('click', toggleMenu);
+  function toggleMenu() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refsmenu.menu.classList.toggle('is-open');
+  }
+})();
